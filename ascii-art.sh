077bb8fd531e6cdd46ff41c24ef9e3aa35a209b3
bc <ahoy-glory-deluge@duck.com>
2025-04-30 14:10:47 -0700

    update script with dragon text

--- ascii-art.sh
+++ ascii-art.sh
@@ -3,5 +3,5 @@ echo "My first github actions job"
 ls -altr
 cat README.md
 sudo apt-get install cowsay
-cowsay -f dragon "Run for cover" >> dragon.txt
+cowsay -f dragon "Dragon - Run for cover" >> dragon.txt
 cat dragon.txt
